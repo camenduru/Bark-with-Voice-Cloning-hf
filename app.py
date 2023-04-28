b67aeeccdd1f57abbb48eb1ca3421cfa59b1f074
Kevin Wang <kevinwang676@users.noreply.huggingface.co>
2023-04-28 16:47:35 +0000

    Update app.py

--- app.py
+++ app.py
@@ -193,6 +193,15 @@ for lang, code in SUPPORTED_LANGS:
     for n in range(10):
         speakers_list.append(f"{code}_speaker_{n}")
 
+examples1 = [
+
+    ["Hello, my name is Suno. And, uh — and I like pizza. [laughs] But I also have other interests such as playing tic tac toe.",
+        "Speaker 1 (en)", 0.7, 0.7]
+
+]
+
+examples2 = [["test.wav", "Bark.wav", "Bark.wav"]]
+
 with gr.Blocks() as demo:
     gr.Markdown(
             f""" # <center>🐶🎶🥳 - Bark with Voice Cloning</center>
@@ -220,7 +229,7 @@ with gr.Blocks() as demo:
         )
     with gr.Row().style(equal_height=True):
 
-        inp2 = gr.Dropdown(speakers_list, value=speakers_list[0], label="Acoustic Prompt")
+        inp2 = gr.Dropdown(speakers_list, value=speakers_list[1], label="Acoustic Prompt")
 
         button = gr.Button("Generate using Bark")
         
@@ -238,7 +247,12 @@ with gr.Blocks() as demo:
         out2 = gr.Audio(label="Generated Audio in a Custom Voice")
 
     btn.click(voice_conversion, [inp5, inp6, inp7], [out2])
-  
+
+    gr.Examples(examples=examples1, fn=generate_text_to_speech, inputs=[inp1, inp2, inp3, inp4],
+                outputs=[out1], cache_examples=True)
+    gr.Examples(examples=examples2, fn=voice_conversion, inputs=[inp5, inp6, inp7],
+                outputs=[out2], cache_examples=True)
+    
     gr.Markdown(
             """ ### <center>NOTE: Please do not generate any audio that is potentially harmful to any person or organization❗</center>
                         
